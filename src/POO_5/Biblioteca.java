551a1d0b3900f882f93444b33cc21e5d1614f2ff
Agoitz <aezkerrabe21dw@ikzubirimanteo.com>
2021-11-23 09:50:40 +0100

    Ej5(empezado)

--- src/POO_5/Biblioteca.java
+++ src/POO_5/Biblioteca.java
@@ -1,5 +1,28 @@
 package POO_5;
 
 public class Biblioteca {
+	
 
+	public static boolean espar (int n) {
+		if(n %2==0) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+
+	public static double cantidaddigitos(int n) {
+		 do{
+	            System.out.print("Introduce un número entero: ");
+	            n = (int) Console.readDouble();
+	            int cifras = 0;    //esta variable es el contador de cifras
+	            while(n!=0){             //mientras a n le queden cifras
+	                    n = n/10;         //le quitamos el último dígito
+	                   cifras++;          //sumamos 1 al contador de cifras
+	            }
+		 	}
+	
+
+		}
+	
 }
---
+++ src/POO_5/Prueba.java
@@ -0,0 +1,10 @@
+package POO_5;
+
+public class Prueba {
+
+	public static void main(String[] args) {
+		// TODO Esbozo de método generado automáticamente
+
+	}
+
+}
